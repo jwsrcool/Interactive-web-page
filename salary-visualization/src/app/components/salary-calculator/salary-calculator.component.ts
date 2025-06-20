222a24de74d9a6c5379961aab2c539574faa3ce1
Vera Westra <jwsrcool@gmail.com>
2025-06-20 09:26:30 +0200

    initial setup

---
+++ salary-visualization/src/app/components/salary-calculator/salary-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-calculator',
+  imports: [],
+  templateUrl: './salary-calculator.component.html',
+  styleUrl: './salary-calculator.component.css'
+})
+export class SalaryCalculatorComponent {
+
+}
---
+++ salary-visualization/src/app/components/visualization/visualization.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualization',
+  imports: [],
+  templateUrl: './visualization.component.html',
+  styleUrl: './visualization.component.css'
+})
+export class VisualizationComponent {
+
+}
