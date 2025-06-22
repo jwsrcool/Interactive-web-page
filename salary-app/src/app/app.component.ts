e0020e5a3b630e0f0bebfcaecd8ea5f28fdb0243
Vera Westra <jwsrcool@gmail.com>
2025-06-22 22:11:43 +0200

    first working product

--- salary-app/src/app/app.component.ts
+++ salary-app/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+
+import { SalaryFormComponent } from './components/salary-form/salary-form.component';
+import { SalaryResultComponent } from './components/salary-result/salary-result.component';
+import { SalaryDataService } from './services/salary-data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ SalaryFormComponent, SalaryResultComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'salary-app';
+  filteredSalaries: number[] = [];
+  private salaryDataService: SalaryDataService = inject(SalaryDataService);
+
+
+  onFormSubmit(selection: { country: string; language: string; experience: string }) {
+    console.log('Form selection received:', selection);
+    this.salaryDataService
+      .getSalaries(selection.country, selection.language)
+      .subscribe((salaries) => {
+        console.log('Salaries fetched:', salaries);
+        this.filteredSalaries = salaries;
+      });
+  }
+
 }
